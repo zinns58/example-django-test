64aa25321b10d75d48f143af8fa5737f49fba1d3
zinns58 <zinns58@gmail.com>
2021-05-07 22:40:28 +0900

    - docker 실행시에 unit test 실행

--- project/entrypoint.sh
+++ project/entrypoint.sh
@@ -3,6 +3,7 @@
 # python manage.py flush --no-input
 python manage.py collectstatic --settings=config.settings.deploy --no-input
 python manage.py migrate --settings=config.settings.deploy
+python manage.py test --settings=config.settings.deploy
 gunicorn config.wsgi.deploy -b 0.0.0.0:8000
 
 exec "$@"
